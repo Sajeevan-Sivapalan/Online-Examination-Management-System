c13945cd93be486b162a2eae1f80c4a1b5e6c03e
Sajeevan2001 <sajeesiva12@gmail.com>
2022-11-12 06:20:49 +0530

    Final Project

---
+++ src/com/student/dao/StudentDaoInterface.java
@@ -0,0 +1,10 @@
+package com.student.dao;
+
+import com.student.model.Student;
+
+public interface StudentDaoInterface {
+	public Student getStudent(Object object);
+
+	public Student updateStudent(int uid, String sname, String address, String dob);
+
+}
